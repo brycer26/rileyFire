6eda66f03ea7f2a80dc45062aed5e7d5daa13268
brycer26 <rileyj3@berea.edu>
2025-12-28 21:04:18 -0500

    added cute photo of odie to catch attention. wrapped the carosel and photo in a <box tag to save up some room. will add more under

--- my-react-app/src/pages/home.jsx
+++ my-react-app/src/pages/home.jsx
@@ -1,7 +1,6 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import SimpleCarousel from '../components/carousel';
-
 const HomePage = () => {
   return (
     <Box
@@ -16,10 +15,37 @@ const HomePage = () => {
         boxSizing: 'border-box',
       }}
     >
-      <Typography variant="h1" color="black" sx={{ fontSize: '2rem', fontWeight: 'bold', marginBottom: '2rem' }}>Welcome to Riley Fire and Safety</Typography>
-      <SimpleCarousel />
-    </Box>
-  );
-};
+      <Typography variant="body1" color="black" 
+      sx={{ 
+        fontSize: '2.5rem', 
+        fontWeight: 'bold', 
+        marginBottom: '2rem' }}>
+          Join the Riley Fire and Safety <br /> family today!
+          </Typography>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              gap: '4rem',
+              alignItems: 'center',
+              justifyContent: 'center',
+              flexWrap: 'wrap',
+            }}
+          >
+            <img 
+              src="/dog.jpg" 
+              alt="Dog Photo" 
+              style={{ 
+                width: '500px',
+                height: 'auto', 
+                borderRadius: '8px',
+                boxShadow: '0 4px 6px rgba(0,0,0,0.1)'
+              }} 
+            />
+            <SimpleCarousel />
+          </Box>
+        </Box>
+    );
+}
 
 export default HomePage;
